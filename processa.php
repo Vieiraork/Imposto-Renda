68a3a1afc59a1f7cc3061747487461e8accfa34f
WilliamVie <vieiraork@gmail.com>
2020-06-06 19:56:37 -0300

    First commit

---
+++ processa.php
@@ -0,0 +1,94 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+		<link rel="stylesheet" href="style.css">
+	</head>
+<body class="fundo">
+	<center>
+		<div class="container container-fluid p-4 rounded p-4 my-3 border" style="width: 500px;">
+<?php
+	class Processa{
+		private $minimo;
+		private $dependentes;
+		private $salario;
+		private $imposto;
+		public $calculoprevio;
+		public $calculoreal;
+		
+	//-------------------GET e SET-------------------
+		function getMinino(){
+			return $this->minimo;
+		}
+		function setMinimo($minimo){
+			$this->minimo=$minimo;
+		}
+		function getDependentes(){
+			return $this->dependentes;
+		}
+		function setDependentes($dependentes){
+			$this->dependentes=$dependentes;
+		}
+		function getSalario(){
+			return $this->salario;
+		}
+		function setSalario($salario){
+			$this->salario=$salario;
+		}
+		function getImposto(){
+			return $this->imposto;
+		}
+		function setImposto($imposto){
+			$this->imposto=$imposto;
+		}
+	//-------------------Validação-------------------
+		
+		public function calculaImposto($salario, $imposto){
+			$this->setSalario($salario);
+			$this->setImposto($imposto);
+			if($this->getImposto()==0){
+				$calculoprevio=0;
+			}else{
+				$calculoprevio = ($this->getSalario()*$this->getImposto())/100;
+			}
+			
+			if($this->getsalario()<5195){
+				$calculoreal = ($this->getSalario())*0;
+				if($calculoprevio==$calculoreal){
+					echo "Seu imposto continua o mesmo!<br>";
+					echo "Valor do imposto R$ ".$calculoreal."<br>";
+				}else{
+					echo "Seu imposto mudou!<br>";
+					echo "Você pagará agora: R$ ".$calculoreal."<br>";
+				}
+			}else if($this->getSalario()>5194 && $this->getSalario()<12468){
+				$calculoreal = ($this->getSalario())*0.08;
+				if($calculoprevio==$calculoreal){
+					echo "Seu imposto continua o mesmo!<br>";
+					echo "Valor do imposto R$ ".$calculoreal."<br>";
+				}else{
+					echo "Seu imposto mudou!<br>";
+					echo "Você pagará agora: R$ ".$calculoreal."<br>";
+				}
+			}else if($this->getSalario()>12468){
+				$calculoreal = ($this->getSalario())*0.20;
+				if($calculoprevio==$calculoreal){
+					echo "Seu imposto continua o mesmo!<br>";
+					echo "Valor do imposto R$ ".$calculoreal."<br>";
+				}else{
+					echo "Seu imposto mudou!<br>";
+					echo "Você pagará agora: R$ ".$calculoreal."<br>";
+				}
+			}
+		}
+	}
+	//-------------------Criação Objeto-------------------
+	$objeto = new Processa();
+	
+	$objeto->calculaImposto($_POST["salario"], $_POST["imposto"]);
+?>
+			<a href="index.html"><input type="submit" value="Voltar" class="btn btn-outline-primary"></a>
+		</div>
+	</center>
+</body>
+</html>
